221cd32cf6a5246df3dca3079b783776367a3993
bachert <bachert@rptu.de>
2024-01-06 12:23:48 +0100

    hallo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
+// astro.config.mjs
 import { defineConfig } from 'astro/config';
+import netlify from '@astrojs/netlify/functions';
 
-import vercel from "@astrojs/vercel/serverless";
-
-// https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: vercel()
+  adapter: netlify(),
 });
